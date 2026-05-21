c0775032e6def3bccdaa6ea63ed887c9507ebe2f
Nguyễn Kim Đạt <25023210@vnu.edu.vn>
2026-05-21 21:34:59 +0700

    Revert "Merge branch 'main' of https://github.com/25023215-netizen/project"
    This reverts commit 2574cfd9ccf4507af82f5ec772b88f71b607d516, reversing
    changes made to bf31e119e80cff86e3e96d5ebf243bbd6b732c65.

--- auctionweb/src/main/java/com/nhom4project/auctionweb/controller/frontend/AuctionDetailController.java
+++ auctionweb/src/main/java/com/nhom4project/auctionweb/controller/frontend/AuctionDetailController.java
@@ -15,7 +15,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
+import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
@@ -65,7 +65,7 @@ public class AuctionDetailController {
     @FXML private Button endEarlyButton;
     @FXML private Button deleteAuctionButton;
 
-    @FXML private LineChart<Number, Number> priceChart;
+    @FXML private AreaChart<Number, Number> priceChart;
     @FXML private NumberAxis xAxis;
     @FXML private NumberAxis yAxis;
 
